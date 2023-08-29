7dc36d505a76000e066a0780776be0ce7523d73a
Vitor Eller <vitor.eller30@gmail.com>
2023-08-29 13:37:27 -0300

    PT-5 feat(log): add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,31 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaDataServiceModule } from './frameworks/data-services/prisma.module';
 import { UserController } from './controllers/user.controller';
 import { UserUseCasesModule } from './use-cases/user/user-use-case.module';
 import { PassportServiceModule } from './frameworks/auth-services.ts/passport/passport-service.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [PrismaDataServiceModule, PassportServiceModule, UserUseCasesModule],
+  imports: [
+    PrismaDataServiceModule,
+    PassportServiceModule,
+    UserUseCasesModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        customProps: (req, res) => ({
+          context: 'HTTP',
+        }),
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
   const config = new DocumentBuilder()
     .setTitle('Cats example')
