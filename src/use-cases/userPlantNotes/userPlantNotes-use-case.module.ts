24a1ce76abfe738b0730b8fb40715247b2ad02f5
Vitor Eller <vitor.eller30@gmail.com>
2023-09-01 17:30:49 -0300

    PT-13 chore(lint): lint files

--- src/use-cases/userPlantNotes/userPlantNotes-use-case.module.ts
+++ src/use-cases/userPlantNotes/userPlantNotes-use-case.module.ts
@@ -1,11 +1,11 @@
-import { PrismaDataServiceModule } from "src/frameworks/data-services/prisma.module";
-import { UserPlantNotesFactoryService } from "./userPlantNotes-factory.service";
-import { UserPlantNotesUseCases } from "./userPlantNotes.use-case";
-import { Module } from "@nestjs/common";
+import { PrismaDataServiceModule } from 'src/frameworks/data-services/prisma.module';
+import { UserPlantNotesFactoryService } from './userPlantNotes-factory.service';
+import { UserPlantNotesUseCases } from './userPlantNotes.use-case';
+import { Module } from '@nestjs/common';
 
 @Module({
-    imports: [PrismaDataServiceModule],
-    providers: [UserPlantNotesFactoryService, UserPlantNotesUseCases],
-    exports:[UserPlantNotesFactoryService, UserPlantNotesUseCases],
+  imports: [PrismaDataServiceModule],
+  providers: [UserPlantNotesFactoryService, UserPlantNotesUseCases],
+  exports: [UserPlantNotesFactoryService, UserPlantNotesUseCases],
 })
 export class UserPlantNotesUseCasesModule {}
